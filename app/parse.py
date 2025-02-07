54c0123f600f1d0bb0308b469663220aba012ef5
Gibbs <mr.darmstadtiumgmail.com>
2025-02-07 15:19:08 +0100

    All working correctly, exept tests

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,17 @@
-from dataclasses import dataclass
+import csv
+import requests
+
+from bs4 import BeautifulSoup, Tag
+
+from dataclasses import dataclass, fields, astuple
+
+BASE_URL = "https://quotes.toscrape.com/"
+
+
+def get_url(
+    base: str = BASE_URL, page_num: int = 1
+) -> str:
+    return base + f"page/{page_num}/"
 
 
 @dataclass
@@ -8,8 +21,41 @@ class Quote:
     tags: list[str]
 
 
+def parse_single_quote(quote: Tag) -> Quote:
+    text = quote.select_one(".text").text,
+    author = quote.select_one(".author").text,
+    tags = [tag.text for tag in quote.select_one(".tags").select(".tag")]
+    quote = Quote(
+        text=quote.select_one(".text").text,
+        author=quote.select_one(".author").text,
+        tags=[tag.text for tag in quote.select_one(".tags").select(".tag")]
+    )
+    return quote
+
+
+def get_quotes(page_number: int = 1) -> [Quote]:
+
+    data = requests.get(get_url(BASE_URL, page_number)).text
+    soup = BeautifulSoup(data, "html.parser")
+    quotes = soup.select(".quote")
+    all_quotes = [parse_single_quote(quote) for quote in quotes]
+
+    while soup.select_one(".next"):
+        page_number += 1
+        data = requests.get(get_url(BASE_URL, page_number)).text
+        soup = BeautifulSoup(data, "html.parser")
+        quotes = soup.select(".quote")
+        all_quotes.extend([parse_single_quote(quote) for quote in quotes])
+
+    return all_quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_quotes(1)
+    with open(output_csv_path, "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow([field.name for field in fields(Quote)])
+        writer.writerows([astuple(quote) for quote in quotes])
 
 
 if __name__ == "__main__":
